967ff0b24dbe21769766fc4929733f15f008886b
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-24 20:10:58 -0700

    added script and cmake

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd lib
+
+git clone https://github.com/mongodb/mongo-c-driver.git
+cd mongo-c-driver
+git checkout 1.21.2
+mkdir cmake-build
+cd cmake-build
+cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
+if [[ "$OSTYPE" =~ ^msys ]]; then
+    sudowin cmake --build . --target install
+else
+    sudo cmake --build . --target install
+fi
+
+cd ../../
+rm -rf mongo-c-driver*
+tar -xf mongo-cxx-driver-r3.6.7.tar.gz
+cd mongo-cxx-driver-r3.6.7/build
+cmake ..                                \
+    -DCMAKE_BUILD_TYPE=Release          \
+    -DBSONCXX_POLY_USE_MNMLSTC=1        \
+    -DCMAKE_INSTALL_PREFIX=/usr/local
+if [[ "$OSTYPE" =~ ^msys ]]; then
+    sudowin cmake --build . --target install
+else
+    sudo cmake --build . --target install
+fi
+cd ../../
+rm -rf mongo-cxx-driver-r3.6.7*
